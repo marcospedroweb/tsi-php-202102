a91c5af3410efbece615537079ce0992b9a9e4e5
GomyBr <king.frontweb@outlook.com>
2021-09-02 20:38:21 -0300

    var_dump

--- vetorExercicio.php
+++ vetorExercicio.php
@@ -7,6 +7,8 @@ $dias['quinta'] = 'linguagens de servidor';
 $dias['sexta'] = 'linguagens de script para web';
 echo "Durante a semana eu tenho as seguintes disciplinas<br><br>";
 
+var_dump($dias);//Ajuda na depuração do codigo, mostra por completo o array
+
 foreach($dias as $dia => $aula){
     //$dias(vetor) as $dia(indice do vetor) => $aula(valor naquele indice)
     echo "<br>$dia tenho aula de $aula<br>";
